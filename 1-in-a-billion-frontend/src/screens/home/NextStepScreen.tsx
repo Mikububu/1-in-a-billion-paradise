868e8c7407f624f4199d65bf2322293d9ebeb8f3
Mikububu <114276760+Mikububu@users.noreply.github.com>
2026-01-10 03:37:53 +0700

    Revert "Increase video background height on NextStepScreen from 35% to 50% for better visibility"
    This reverts commit 4ead9606e0e7ca617a4af679d7dce8fcba2e0829.

--- 1-in-a-billion-frontend/src/screens/home/NextStepScreen.tsx
+++ 1-in-a-billion-frontend/src/screens/home/NextStepScreen.tsx
@@ -99,7 +99,7 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     width: SCREEN_WIDTH,
-    height: SCREEN_HEIGHT * 0.50, // Fill bottom half
+    height: SCREEN_HEIGHT * 0.35, // Smaller height, flush to bottom
     zIndex: 1,
   },
   buttonsContainer: {
