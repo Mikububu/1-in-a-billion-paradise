ec7f5d800ffe3af6bb40b05d65ea44daa1a0700e
Michael <michael@forbidden-yoga.com>
2026-03-04 20:41:12 +0400

    Auto-deploy updates from desktop

--- 1-in-a-billion-v2/backend/check_llm.ts
+++
@@ -1,5 +0,0 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-console.log('PAID_LLM_PROVIDER:', process.env.PAID_LLM_PROVIDER);
-console.log('CLAUDE_MODEL:', process.env.CLAUDE_MODEL);
--- 1-in-a-billion-v2/backend/test-regex.js
+++
@@ -1,7 +0,0 @@
-const text = "This is a reading about Gene Keys and Human Design. How exciting!";
-const original = "Gene Keys";
-// Correct escape:
-const escaped = original.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-const regex = new RegExp(`\\b${escaped}\\b`, "gi");
-console.log("Regex:", regex);
-console.log("Result:", text.replace(regex, "Dschien Kies"));
